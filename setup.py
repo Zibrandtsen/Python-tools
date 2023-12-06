10b3d2bd3f5adea2ad1d94ce5e81a46eba2ebe47
Zibrandtsen <72438408+Zibrandtsen@users.noreply.github.com>
2023-12-06 15:00:24 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name="python_tools_Zibrandtsen",
+      version="1.0.0",
+      description="Some usefull tools for python development",
+      license='MIT',
+      author="Jacob Zibrandtsen",
+      authoer_email="jrz@live.dk",
+      url='https://github.com/Zibrandtsen/Python-tools'
+      packages=['csv_tools']
+)
